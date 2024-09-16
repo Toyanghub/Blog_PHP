76bb0d4a26d9dd24ea7eed0ea1e70a86311d61b4
Your Name <selwyntayong12@gmail.com>
2024-09-16 15:45:01 +0800

    Done in Contents 2. Using real data

--- install.php
+++ install.php
@@ -1,14 +1,18 @@
 <?php
+require_once 'lib/common.php';
+
 // Get the PDO DSN string
-$root = realpath(__DIR__);
-$database = $root . '/data/data.sqlite';
-$dsn = 'sqlite:' . $database;
+$root = getRootPath();
+$database = getDatabasePath();
+
 $error = '';
+
 // A security measure, to avoid anyone resetting the database if it already exists
 if (is_readable($database) && filesize($database) > 0)
 {
     $error = 'Please delete the existing database manually before installing it afresh';
 }
+
 // Create an empty file for the database
 if (!$error)
 {
@@ -21,25 +25,29 @@ if (!$error)
         );
     }
 }
+
 // Grab the SQL commands we want to run on the database
 if (!$error)
 {
     $sql = file_get_contents($root . '/data/init.sql');
+
     if ($sql === false)
     {
         $error = 'Cannot find SQL file';
     }
 }
+
 // Connect to the new database and try to run the SQL commands
 if (!$error)
 {
-    $pdo = new PDO($dsn);
+    $pdo = getPDO();
     $result = $pdo->exec($sql);
     if ($result === false)
     {
         $error = 'Could not run SQL: ' . print_r($pdo->errorInfo(), true);
     }
 }
+
 // See how many rows we created, if any
 $count = null;
 if (!$error)
@@ -51,6 +59,7 @@ if (!$error)
         $count = $stmt->fetchColumn();
     }
 }
+
 ?>
 <!DOCTYPE html>
 <html>
